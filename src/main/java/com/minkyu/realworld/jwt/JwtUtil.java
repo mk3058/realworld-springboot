b5204183878f380b20c297f092c3bdb02e7f806c
mk3058 <kmk3058@icloud.com>
2024-02-28 15:09:11 +0900

    feat(jwt): add jwt classes

---
+++ src/main/java/com/minkyu/realworld/jwt/JwtUtil.java
@@ -0,0 +1,60 @@
+package com.minkyu.realworld.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("jwt")
+@Setter
+public class JwtUtil {
+
+    private String secret;
+    
+    private Long accessTokenValidityInSeconds;
+
+    public String getUsername(String token) {
+
+        return Jwts.parserBuilder()
+            .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret))).build()
+            .parseClaimsJws(token).getBody()
+            .get("username", String.class);
+    }
+
+    public String getRole(String token) {
+
+        return Jwts.parserBuilder()
+            .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret))).build()
+            .parseClaimsJws(token).getBody()
+            .get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+
+        return Jwts.parserBuilder()
+            .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret))).build()
+            .parseClaimsJws(token).getBody()
+            .getExpiration().before(new Date());
+    }
+
+    public String createJwt(String username, String role) {
+
+        Claims claims = Jwts.claims();
+        claims.put("username", username);
+        claims.put("role", role);
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(
+                new Date(System.currentTimeMillis() + accessTokenValidityInSeconds * 1000))
+            .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret)), SignatureAlgorithm.HS256)
+            .compact();
+    }
+}
