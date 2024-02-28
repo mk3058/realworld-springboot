0f89fbe038e4028f26b94dad4ada0b40eb7306bc
mk3058 <kmk3058@icloud.com>
2024-02-28 17:52:59 +0900

    feat(UserController): add getCurrentUser, getProfile endpoints

--- src/main/java/com/minkyu/realworld/user/application/UserService.java
+++ src/main/java/com/minkyu/realworld/user/application/UserService.java
@@ -1,15 +1,61 @@
 package com.minkyu.realworld.user.application;
 
+import com.minkyu.realworld.common.exception.UserNotFoundException;
+import com.minkyu.realworld.follow.domain.repository.FollowRepository;
+import com.minkyu.realworld.jwt.CustomUserDetails;
+import com.minkyu.realworld.user.domain.User;
 import com.minkyu.realworld.user.domain.repository.UserRepository;
-import com.minkyu.realworld.user.presentation.UserController;
+import com.minkyu.realworld.user.presentation.dto.ProfileResponse;
+import jakarta.security.auth.message.AuthException;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
 public class UserService {
 
     private final UserRepository userRepository;
+    private final FollowRepository followRepository;
+
+    public ProfileResponse findCurrentUser() throws Exception {
+        CustomUserDetails userDetails = findAuthenticatedUser();
+        User user = userRepository.findByUsername(userDetails.getUsername())
+            .orElseThrow(() -> new UserNotFoundException("Cannot find current user!"));
+
+        return ProfileResponse.fromEntity(user, false);
+    }
+
+    public ProfileResponse findUserByUsername(String username) throws Exception {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UserNotFoundException("Cannot find user " + username));
+
+        return ProfileResponse.fromEntity(user, isFollower(user));
+    }
+
+    private Boolean isFollower(User target) throws Exception {
+        CustomUserDetails userDetails = findAuthenticatedUser();
+        User current = userRepository.findByUsername(userDetails.getUsername()).orElseThrow(
+            NotFoundException::new);
+
+        return followRepository.existsByFollowerIdAndFolloweeId(current.getId(), target.getId());
+    }
+
+    private CustomUserDetails findAuthenticatedUser() throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AuthException("User not authenticated!");
+        }
+
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof CustomUserDetails) {
+            return (CustomUserDetails) principal;
+        }
+        throw new UserPrincipalNotFoundException("Cannot find UserDetails!");
+    }
+
 }
--- src/main/java/com/minkyu/realworld/user/presentation/UserController.java
+++ src/main/java/com/minkyu/realworld/user/presentation/UserController.java
@@ -1,13 +1,41 @@
 package com.minkyu.realworld.user.presentation;
 
+import com.minkyu.realworld.common.error.ErrorResponse;
 import com.minkyu.realworld.user.application.UserService;
-import com.minkyu.realworld.user.domain.repository.UserRepository;
+import com.minkyu.realworld.user.presentation.dto.ProfileResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/user")
+    public ResponseEntity<?> getCurrentUser() {
+        try {
+            ProfileResponse profileResponse = userService.findCurrentUser();
+            return ResponseEntity.ok(profileResponse);
+        } catch (Exception e) {
+            return ResponseEntity.unprocessableEntity()
+                .body(ErrorResponse.fromMessage(e.getMessage()));
+        }
+    }
+
+    @GetMapping("/profiles/{username}")
+    public ResponseEntity<?> getProfile(@PathVariable String username) {
+        try {
+            ProfileResponse profileResponse = userService.findUserByUsername(username);
+            return ResponseEntity.ok(profileResponse);
+        } catch (Exception e) {
+            return ResponseEntity.unprocessableEntity()
+                .body(ErrorResponse.fromMessage(e.getMessage()));
+        }
+    }
 }
