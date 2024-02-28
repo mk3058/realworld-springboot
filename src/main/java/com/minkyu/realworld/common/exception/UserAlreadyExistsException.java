46a3b4f7fa4b85f58cd5c7d770ff8dc8e1949fdf
mk3058 <kmk3058@icloud.com>
2024-02-28 15:07:17 +0900

    feat(Exception): add UserAlreadyExistsException

---
+++ src/main/java/com/minkyu/realworld/common/exception/UserAlreadyExistsException.java
@@ -0,0 +1,20 @@
+package com.minkyu.realworld.common.exception;
+
+public class UserAlreadyExistsException extends Exception {
+
+    public UserAlreadyExistsException() {
+        super();
+    }
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
+}
