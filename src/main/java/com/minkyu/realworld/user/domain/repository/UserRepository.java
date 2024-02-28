4600c4c3eb119a252b26735faff7f7f75c5dfdd7
mk3058 <kmk3058@icloud.com>
2024-02-28 15:04:19 +0900

    feat(UserRepository): add existsByEmail, existsByUsername methods

--- src/main/java/com/minkyu/realworld/user/domain/repository/UserRepository.java
+++ src/main/java/com/minkyu/realworld/user/domain/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package com.minkyu.realworld.user.domain.repository;
 
 import com.minkyu.realworld.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+    Boolean existsByUsername(String username);
 }
