6646a07714ca0871fa977b6073f5d09e90330d1f
mk3058 <kmk3058@icloud.com>
2024-02-28 15:12:09 +0900

    feat: add auth dtos

---
+++ src/main/java/com/minkyu/realworld/auth/presentation/dto/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.minkyu.realworld.auth.presentation.dto;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+@JsonTypeName("user")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+public record LoginRequest(String email, String password) {
+
+}
