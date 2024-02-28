ff38973bfb9a0dd97338c1b5be333ff17ace7bb1
mk3058 <kmk3058@icloud.com>
2024-02-28 17:39:50 +0900

    feat(ProfileResponse): add Json TypeName mapping statement

--- src/main/java/com/minkyu/realworld/user/presentation/dto/ProfileResponse.java
+++ src/main/java/com/minkyu/realworld/user/presentation/dto/ProfileResponse.java
@@ -1,9 +1,12 @@
 package com.minkyu.realworld.user.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.minkyu.realworld.user.domain.User;
-import java.util.UUID;
 
-public record ProfileResponse(String username, String bio, UUID image, Boolean following) {
+@JsonTypeName("user")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
+public record ProfileResponse(String username, String bio, String image, Boolean following) {
 
     public static ProfileResponse fromEntity(User target, Boolean following) {
         return new ProfileResponse(target.getUsername(), target.getBio(), target.getImage(),
