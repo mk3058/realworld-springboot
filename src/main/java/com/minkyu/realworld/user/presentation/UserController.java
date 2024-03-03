cf15d0b8bfa9d2f2b92db02784083c5741bc3f32
mk3058 <kmk3058@icloud.com>
2024-03-03 14:57:29 +0900

    feat(UserController): add updateUser endpoint

--- src/main/java/com/minkyu/realworld/user/presentation/UserController.java
+++ src/main/java/com/minkyu/realworld/user/presentation/UserController.java
@@ -3,11 +3,14 @@ package com.minkyu.realworld.user.presentation;
 import com.minkyu.realworld.common.error.ErrorResponse;
 import com.minkyu.realworld.user.application.UserService;
 import com.minkyu.realworld.user.presentation.dto.ProfileResponse;
+import com.minkyu.realworld.user.presentation.dto.UserUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,6 +35,17 @@ public class UserController {
     public ResponseEntity<?> getProfile(@PathVariable("username") String username) {
         try {
             ProfileResponse profileResponse = userService.findUserByUsername(username);
+            return ResponseEntity.ok(profileResponse);
+        } catch (Exception e) {
+            return ResponseEntity.unprocessableEntity()
+                .body(ErrorResponse.fromMessage(e.getMessage()));
+        }
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<?> updateUser(@RequestBody UserUpdateRequest dto) {
+        try {
+            ProfileResponse profileResponse = userService.updateCurrentUser(dto);
             return ResponseEntity.ok(profileResponse);
         } catch (Exception e) {
             return ResponseEntity.unprocessableEntity()
