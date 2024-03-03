4ce3bd20b281861d205fafcc983eafe190d5f291
mk3058 <kmk3058@icloud.com>
2024-03-03 15:59:54 +0900

    feat(FollowService): add createByUsername, deleteByUsername methods

--- src/main/java/com/minkyu/realworld/follow/application/FollowService.java
+++ src/main/java/com/minkyu/realworld/follow/application/FollowService.java
@@ -1,7 +1,14 @@
 package com.minkyu.realworld.follow.application;
 
+import com.minkyu.realworld.auth.application.AuthService;
+import com.minkyu.realworld.common.exception.UserNotFoundException;
+import com.minkyu.realworld.follow.domain.Follow;
 import com.minkyu.realworld.follow.domain.repository.FollowRepository;
+import com.minkyu.realworld.jwt.CustomUserDetails;
 import com.minkyu.realworld.user.domain.User;
+import com.minkyu.realworld.user.domain.repository.UserRepository;
+import com.minkyu.realworld.user.presentation.dto.ProfileResponse;
+import com.sun.jdi.request.DuplicateRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +17,42 @@ import org.springframework.stereotype.Service;
 public class FollowService {
 
     private final FollowRepository followRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+
+    public ProfileResponse createByUsername(String followeeName) throws Exception {
+        CustomUserDetails userDetails = authService.findAuthenticatedUser();
+        User follower = userRepository.findByUsername(userDetails.getUsername())
+            .orElseThrow(() -> new UserNotFoundException("Cannot find current user!"));
+        User followee = userRepository.findByUsername(followeeName)
+            .orElseThrow(() -> new UserNotFoundException("Cannot find user named" + followeeName));
+
+        if (followRepository.existsByFollowerAndFollowee(follower, followee)) {
+            throw new DuplicateRequestException(follower.getUsername() + " is already following  " +
+                followee.getUsername());
+        }
+        Follow follow = new Follow(follower, followee);
+        followRepository.save(follow);
+        return ProfileResponse.fromEntity(followee, isFollower(follower, followee));
+    }
+
+    public ProfileResponse deleteByUsername(String followeeName) throws Exception {
+        CustomUserDetails userDetails = authService.findAuthenticatedUser();
+        User follower = userRepository.findByUsername(userDetails.getUsername())
+            .orElseThrow(() -> new UserNotFoundException("Cannot find current user!"));
+        User followee = userRepository.findByUsername(followeeName)
+            .orElseThrow(() -> new UserNotFoundException("Cannot find user named" + followeeName));
+
+        if (!followRepository.existsByFollowerAndFollowee(follower, followee)) {
+            throw new IllegalArgumentException(follower.getUsername() + " is not a follower of " +
+                followee.getUsername());
+        }
+        followRepository.deleteByFollowerAndFollowee(follower, followee);
+        return ProfileResponse.fromEntity(followee, isFollower(follower, followee));
+    }
 
     public Boolean isFollower(User follower, User followee) throws Exception {
-        return followRepository.existsByFollowerIdAndFolloweeId(follower.getId(), followee.getId());
+        return followRepository.existsByFollowerAndFollowee(follower, followee);
     }
 
 }
