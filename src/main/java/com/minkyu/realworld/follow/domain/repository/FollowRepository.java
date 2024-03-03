8d0cd16513136c51f9d38361b3e1e16ac30c4fe9
mk3058 <kmk3058@icloud.com>
2024-03-03 15:58:33 +0900

    feat(FollowRepository): add deleteByFollowerAndFollowee method

--- src/main/java/com/minkyu/realworld/follow/domain/repository/FollowRepository.java
+++ src/main/java/com/minkyu/realworld/follow/domain/repository/FollowRepository.java
@@ -1,9 +1,12 @@
 package com.minkyu.realworld.follow.domain.repository;
 
 import com.minkyu.realworld.follow.domain.Follow;
+import com.minkyu.realworld.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
-    Boolean existsByFollowerIdAndFolloweeId(Long follower, Long Followee);
+    void deleteByFollowerAndFollowee(User follower, User followee);
+
+    Boolean existsByFollowerAndFollowee(User follower, User followee);
 }
