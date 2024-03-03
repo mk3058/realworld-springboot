3ab37d073a9cb871ef886c7af19f3001ee15bbda
mk3058 <kmk3058@icloud.com>
2024-03-03 15:59:13 +0900

    feat(Follow): add constructor

--- src/main/java/com/minkyu/realworld/follow/domain/Follow.java
+++ src/main/java/com/minkyu/realworld/follow/domain/Follow.java
@@ -1,5 +1,6 @@
 package com.minkyu.realworld.follow.domain;
 
+import com.google.common.base.Preconditions;
 import com.minkyu.realworld.user.domain.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,10 +8,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Follow {
 
     @Id
@@ -24,4 +28,14 @@ public class Follow {
     @ManyToOne
     @JoinColumn(name = "followee_id")
     private User followee;
+
+    public Follow(User follower, User followee) {
+        Preconditions.checkNotNull(follower, "follower must be provided");
+        Preconditions.checkNotNull(followee, "followee must be provided");
+        Preconditions.checkArgument(!follower.getId().equals(followee.getId()),
+            "follower and followee cannot be same");
+        
+        this.follower = follower;
+        this.followee = followee;
+    }
 }
