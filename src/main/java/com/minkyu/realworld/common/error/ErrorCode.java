7ef4fa1b9dc5682076c006f68800816bef3b282a
mk3058 <kmk3058@icloud.com>
2024-03-04 15:58:21 +0900

    feat(ErrorCode): add UNAUTHORIZED, FORBIDDEN error code

--- src/main/java/com/minkyu/realworld/common/error/ErrorCode.java
+++ src/main/java/com/minkyu/realworld/common/error/ErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATE_USER(HttpStatus.UNPROCESSABLE_ENTITY, "User already exists"),
-    USER_NOT_FOUND(HttpStatus.UNPROCESSABLE_ENTITY, "User not found");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "Unauthorized request"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "Permission denied");
 
     private final HttpStatus status;
     private final String message;
